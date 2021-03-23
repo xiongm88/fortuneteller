6834e9cd42dc68360e12f87be3ba549747888899
Monday Xiong <brewcitygoon@brewcitygoons.lan>
2021-03-23 01:03:19 -0500

    comments on video

--- fortuneteller/fortune.js
+++ fortuneteller/fortune.js
@@ -14,9 +14,11 @@ if (result != null) {
 	alert("What would you like to know?!");
 }
 
-// "The client server model" client makes request, server responds to request. 
-// Front-end: 
-// HTML: holds structure and contents, CSS: how to style and arrange webpage, JavaScript: how webpage should behave and its interactions 
-// Back-end:
-// Retrieving data from database, works on functionality. Uses programming languages that serve requests(JavaScript, Java, Python, PHP, etc)
-// Both Front-end and Back-end will need knowledge of HTML, CSS, JavaScript and how they present webpages.
+/*
+"The client server model" client makes request, server responds to request. 
+Front-end: 
+HTML: holds structure and contents, CSS: how to style and arrange webpage, JavaScript: how webpage should behave and its interactions 
+Back-end:
+Retrieving data from database, works on functionality. Uses programming languages that serve requests(JavaScript, Java, Python, PHP, etc)
+Both Front-end and Back-end will need knowledge of HTML, CSS, JavaScript and how they present webpages.
+*/
