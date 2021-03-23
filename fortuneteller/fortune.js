c0a581e1418e747adff561bf06d317dc00f1319b
Monday Xiong <78505410+xiongm88@users.noreply.github.com>
2021-03-23 00:21:58 -0500

    Starting

---
+++ fortuneteller/fortune.js
@@ -0,0 +1,17 @@
+// Prompt to ask a question
+let fortune = prompt('Get your fortune! What would you like to know?');
+
+// Results
+const result = ["Signs point to yes!", "Very likely", "Maybe", "Outcome not so good", "Please ask again later"]
+
+// Randomizer
+const random = Math.floor(Math.random() * result.length);
+
+// Results posted
+if (result != null) {
+	alert(result[random]);
+} else {
+	alert("What would you like to know?!");
+}
+
+// 
